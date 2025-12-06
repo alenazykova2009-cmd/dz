aa0c67bfbe2f354a575e6d4f39a14fc015bc6968
alenazykova2009-cmd <alenazykova2009@mail.ru>
2025-12-06 23:13:22 +0800

    Add files via upload

--- pyth.py
+++ pyth.py
@@ -4,6 +4,7 @@
 #все возможные комбинации меньших чисел,сумма которых равна исходному числу.Например, для числа 4 результатом могут быть
 #[1, 1, 1, 1], [1, 1, 2], [2, 2], [1, 3] и т.д.
 import random
+
 numbers = []
 
 for _ in range(10):
@@ -17,3 +18,23 @@ def find_max(lst):
     return lst[0] if lst[0] > rec_max else rec_max
 print(numbers)
 print(find_max(numbers))
+
+def get_partitions(n):
+    def helper(target, min_val):
+        if target == 0:
+            return [[]]
+
+        partitions = []
+
+        for i in range(min_val, target + 1):
+            for p in helper(target - i, i):
+                partitions.append([i] + p)
+        return partitions
+    result = []
+    for i in range(1, n):
+        for p in helper(n - i, i):
+            result.append([i] + p)
+
+    return result
+
+print(get_partitions(4))
